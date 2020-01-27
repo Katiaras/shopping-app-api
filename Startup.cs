f43c600bc5f8f867503138995396c113a3cb1b3b
Konstantinos Giouzakov <giouzakovkostas@gmail.com>
2020-01-27 22:27:16 +0100

    Added Cors

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace ShoppingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,8 @@ namespace ShoppingApp.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
